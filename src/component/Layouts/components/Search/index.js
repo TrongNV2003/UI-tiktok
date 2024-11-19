c8780bb8274d970dbab5e1bb127b1bbce81a7606
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-11-19 09:46:45 +0700

    fix: set delay when call api for searching

--- src/component/Layouts/components/Search/index.js
+++ src/component/Layouts/components/Search/index.js
@@ -9,6 +9,7 @@ import { Wrapper as PopperWrapper } from "@app/component/Popper";
 import { SearchIcon } from "@app/component/Icons";
 import AccountItem from "@app/component/AccountItem";
 import styles from "./Search.module.scss";
+import { useDebounce } from "@app/hooks";
 
 const cx = classNames.bind(styles);
 
@@ -18,17 +19,19 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -37,7 +40,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue("");
